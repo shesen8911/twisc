bdabf5355c95005651b3962f07edaa89e085ce7e
shesen8911 <shesen8911@gmail.com>
2022-07-27 00:02:32 +0800

    init project

---
+++ src/main/java/edu/ntust/twisc/EcSimpleApplication.java
@@ -0,0 +1,13 @@
+package edu.ntust.twisc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcSimpleApplication.class, args);
+	}
+
+}
